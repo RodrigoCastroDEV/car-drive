84e2cb981439457c0ef77da97e9dd381a4af376d
Alan Brilhante <alancoutinho.ti@gmail.com>
2020-09-05 20:25:31 -0300

    Mudança de estado do farol

--- car-drive/src/Components/Farol.jsx
+++ car-drive/src/Components/Farol.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './styles/semaforo.scss';
 
-const Farol = ({color, teste}) => {
-    console.log(teste)
+const Farol = ({color, active}) => {
+    // se active for igual a true => -active
+    // se active for igual a false => 
+    const isActive = active ? `-active`: ``
+    // semaforo__farol-red (farol vermelho apagado)
+    // semaforo__farol-red-active (farol vermelho aceso)
     return (
-        <div className={`semaforo__farol-${color}`}>
+        <div className={`semaforo__farol-${color}${isActive}`}>
         
         </div>
     )
--- car-drive/src/Components/Semaforo.jsx
+++ car-drive/src/Components/Semaforo.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './styles/semaforo.scss';
 import Farol from './Farol'
 
 const Semaforo = () => {
+    const [active,setActive] = useState('red')
+    const mudaFarol = ()=>{
+        switch(active) {
+            case 'red':
+                setActive('green')
+                break;
+            case 'orange':
+                setActive('red')
+                break;
+            case 'green':
+                setActive('orange')
+                break;
+        }
+    }
+    setInterval(mudaFarol, 4000)
+   
     return (
         <div className="semaforo-container">
-        <Farol color="red" teste="breja"/>
-        <Farol color="orange"/>
-        <Farol color="green"/>
+        <Farol color="red" active={active === 'red'}/>
+        <Farol color="orange" active={active === 'orange'}/>
+        <Farol color="green" active={active === 'green'}/>
         </div>
     )
 }
